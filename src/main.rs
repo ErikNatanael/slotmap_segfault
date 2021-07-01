d29528876d97ca29ba7c1121fd72e7966d482aaa
Erik Natanael Gustafsson <erik@eriknatanael.com>
2021-07-01 15:27:02 +0200

    Narrow problem down

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,31 @@
 use rand::prelude::*;
-struct DataStruct {
-    pub closure: Box<dyn FnMut() -> f32>,
-}
 
 fn main() {
-    let mut sm = slotmap::DenseSlotMap::with_capacity(10000);
+    let mut sm = slotmap::HopSlotMap::with_capacity(10000);
     let mut keys = Vec::new();
     let mut rng = rand::thread_rng();
+    // Repeat simulate actual usage by going back and forth between inserting and removing
     for _j in 0..20 {
-        for _i in 0..rng.gen::<usize>() % 200 {
-            let mut counter = 0.0;
-            let mut vector = vec![10, 20];
-            let data = DataStruct {
-                closure: Box::new(move || {
-                    counter += vector[0] as f32;
-                    counter += 1.0;
-                    vector[0] += counter as u32;
-                    counter
-                }),
-            };
+        // Insert a random number of new values
+        for _i in 0..(rng.gen::<usize>() % 20 + 1) {
+            // A boxed value causes a segfault or an "abort"
+            let data = Box::new(4);
+            // A non boxed value causes an infinite busy loop
+            // let data = 4;
             keys.push(sm.insert(data));
         }
 
-        for (_key, data) in sm.iter_mut() {
-            print!("{}, ", (data.closure)());
-        }
-
+        // Randomly remove certain elements, probably out of order
         for _k in 0..rng.gen::<usize>() % keys.len() {
             let key = keys.remove(rng.gen::<usize>() % (keys.len() - 1));
             sm.remove(key);
         }
     }
-    sm.clear();
+    // This is where the segfault/abort/busy loop happens. Also happens with clear().
+    println!("Number of elements in map: {}", sm.len());
+    for (i, (key, value)) in sm.drain().enumerate() {
+        // When a crash happens, the number of key value pairs is not the same as the len
+        println!("value num {}", i);
+    }
     println!("Hello, world!");
 }
